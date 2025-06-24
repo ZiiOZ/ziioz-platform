7e351bf23acef626aea3e8e8f8846614a2b818d7
ZiiOZ <support@ziioz.com>
2025-06-24 06:20:40 +0000

    Add placeholder ZiiPostFeed to fix Vercel build

---
+++ components/ZiiPostFeed.tsx
@@ -0,0 +1,8 @@
+export default function ZiiPostFeed() {
+  return (
+    <div style={{ textAlign: "center", padding: "2rem" }}>
+      <h2>ZiiPostFeed Placeholder</h2>
+      <p>This is a temporary version for build testing.</p>
+    </div>
+  );
+}
