d35b23349321b335de75bee541881699944354e1
ZiiOZ <support@ziioz.com>
2025-06-26 05:10:12 +0000

    🔥 Remove Stripe apiVersion to fix build error

--- backend/api/create-stripe-account.ts
+++ backend/api/create-stripe-account.ts
@@ -2,9 +2,8 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 import Stripe from 'stripe';
 import { createClient } from '@supabase/supabase-js';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-04-10',
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
 
 const supabase = createClient(
   process.env.SUPABASE_URL!,
