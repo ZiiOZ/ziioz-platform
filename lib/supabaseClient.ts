87ab779e64e06f2db6223d01432f919a138cbd4e
ZiiOZ <support@ziioz.com>
2025-06-26 04:05:13 +0000

    🐛 Fix: use relative import for supabase types to prevent Vercel build fail

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -2,6 +2,7 @@
 
 import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
 import { type SupabaseClient } from '@supabase/supabase-js';
-import type { Database } from '@/types/supabase';
+import type { Database } from '../types/supabase';
+
 
 export const supabase: SupabaseClient<Database> = createPagesBrowserClient<Database>();
