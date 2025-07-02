cf87f38a35bb750491209b68afb0422306c879f1
ZiiOZ <support@ziioz.com>
2025-07-02 22:30:36 +0000

    Remove apiVersion to fix Stripe TypeScript error

--- app/api/ziipay/onboard/route.ts
+++ app/api/ziipay/onboard/route.ts
@@ -2,9 +2,7 @@ import { NextResponse } from "next/server";
 import Stripe from "stripe";
 import { buffer } from "micro";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2023-10-16",
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 // Disable Next.js body parsing to get raw body
 export const config = {
