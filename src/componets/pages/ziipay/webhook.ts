5a18eeea32eeab0997b039fbe5f00798512da593
ZiiOZ <support@ziioz.com>
2025-07-03 09:21:44 +0000

    Fix Stripe apiVersion to 2025-05-28.basil

--- src/componets/pages/ziipay/webhook.ts
+++ src/componets/pages/ziipay/webhook.ts
@@ -9,8 +9,9 @@ export const config = {
 };
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-10-16',
+  apiVersion: '2025-05-28.basil',
 });
+
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
