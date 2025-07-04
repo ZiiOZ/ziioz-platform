153288b0b07089e2a40e188b6fe2aa4d78d8f48e
ZiiOZ <support@ziioz.com>
2025-07-04 06:05:47 +0000

    Remove apiVersion from webhook Stripe initialization

--- src/componets/pages/ziipay/webhook.ts
+++ src/componets/pages/ziipay/webhook.ts
@@ -8,9 +8,8 @@ export const config = {
   },
 };
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2023-10-16", // Or whichever version you want
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "POST") {
