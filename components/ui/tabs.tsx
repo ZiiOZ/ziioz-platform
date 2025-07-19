15f0ec8ef71ba454b72f5d4dca7dec37b6908296
ZiiOZ <support@ziioz.com>
2025-07-19 06:58:25 +0000

    Fix logo path for homepage preview

--- components/ui/tabs.tsx
+++ components/ui/tabs.tsx
@@ -13,7 +13,7 @@ const TabsList = React.forwardRef<
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      'inline-flex h-10 items-center justify-center rounded-md bg-gray-100 p-1 text-gray-500',
+      'inline-flex h-10 items-center justify-center rounded-lg bg-zinc-100 dark:bg-zinc-900 p-1 text-zinc-500 shadow-sm',
       className
     )}
     {...props}
@@ -28,7 +28,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      'inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-white transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-white data-[state=active]:text-black',
+      'inline-flex items-center justify-center whitespace-nowrap rounded-md px-4 py-2 text-sm font-semibold transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-offset-2 disabled:opacity-50 data-[state=active]:bg-white data-[state=active]:text-black dark:data-[state=active]:bg-zinc-800 dark:data-[state=active]:text-white',
       className
     )}
     {...props}
