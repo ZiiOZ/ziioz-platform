bf3ce5f9c2567fd47a92730e407ee30f2aee6495
ZiiOZ <support@ziioz.com>
2025-07-20 22:11:04 +0000

    Force image to true center on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import styles from './landing.module.css';
 export default function LandingPage() {
   return (
     <main className="min-h-screen flex items-center justify-center px-6 py-12 bg-white text-black">
-      <div className="flex justify-center items-center">
+      <div className="w-full flex justify-center items-center">
         <img
           src="/ziioz-preview.png"
           alt="ZiiOZ Preview"
