7d6c7d37491e1fe60f6362b4104c755484f0084b
ZiiOZ <support@ziioz.com>
2025-07-21 22:33:24 +0000

    Add custom <head> with title, description, favicon

---
+++ app/head.tsx
@@ -0,0 +1,12 @@
+// File: app/head.tsx
+
+export default function Head() {
+  return (
+    <>
+      <title>ZiiOZ</title>
+      <meta name="description" content="ZiiOZ Official Landing Page" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
