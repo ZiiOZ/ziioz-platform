039d806b3864b4e2b535a60f322f6f2c73c69b40
ZiiOZ <support@ziioz.com>
2025-08-06 00:03:18 +0000

    Fix: move PostPreviewCard to correct root components folder

--- src/componets/PaymentStatus.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function PaymentStatus({ status }: { status: string }) {
-  return (
-    <div className="p-4 border rounded bg-gray-100 mt-4">
-      <p>Status: {status}</p>
-    </div>
-  );
-}
--- src/componets/PostPreviewCard.tsx
+++
@@ -1,7 +0,0 @@
-export default function PostPreviewCard() {
-  return (
-    <div className="p-4 border border-gray-300 rounded">
-      <p>Preview content coming soon...</p>
-    </div>
-  );
-}
